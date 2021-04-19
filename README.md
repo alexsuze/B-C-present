331ce7c5daaba0616e142dc59a9d5dc78979a2e8
Alex Su <alex801221@gmail.com>
2021-04-19 18:00:33 +0800

    04191800
    1. finish index.html
    2. finish about.html

--- README.md
+++ README.md
@@ -6,4 +6,9 @@
 
 [2021/04/19]  
 1. Change new path.  
-2. 
+2. (RWD)Fix navigation's composing under 918px.  
+3. (RWD)(Javascript)Add click menu button's event. Open & close navigation and change icon image.  
+4. (RWD)(Javascript)Add click list button's event. Open & close instructors list and change icon image.  
+5. (RWD)Fix main's composing under 918px.  
+6. (RWD)Fix footer's composing under 918px.  
+7. Finish about.html.(included RWD).
--- docs/js/index.js
+++ docs/js/index.js
@@ -3,23 +3,30 @@ window.onload = function () {
     var menu_btn = $('#menu_btn');
     var main_list_rwd = $('.main_list_rwd');
     var menu_btn = $('.menu_btn');
-
+    var expand_btn = $('#expand');
+    var sub_list_rwd = $('.sub_list_rwd');
     var html = "";
 
-    function BtnFn() {
+    function menuBtnFn() {
         main_list_rwd[0].classList.toggle('open_menu');
         if (main_list_rwd[0].classList.contains('open_menu')) {
             menu_btn[0].src = './images/mobile/mobile-close.png';
         } else {
             menu_btn[0].src = './images/mobile/mobile-menu.png';
         }
-        console.log(menu_btn[0]);
+    }
+
+    function listBtnFn() {
+        sub_list_rwd[0].classList.toggle('open_list');
+        if (sub_list_rwd[0].classList.contains('open_list')) {
+            expand_btn[0].src = './images/mobile/mobile-collapse.png';
+        } else {
+            expand_btn[0].src = './images/mobile/mobile-expand.png';
+        }
     }
 
     //menu button click event
-    menu_btn.on('click', function () {
-        BtnFn();
-    });
-
+    menu_btn.on('click', menuBtnFn);
+    expand_btn.on('click', listBtnFn);
 
 }
